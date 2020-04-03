d04803cf79ac06e17f65bd30cdd2a56e476272cc
yoo-mech <usam4760@gmail.com>
2020-04-04 01:29:22 +0900

    대략적인 구조 및 community screen 생각

---
+++ lib/Screens/community/community_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CommunityScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: container 를 list로 생성해야 한다. 포스팅 창으로 넘어가도록 ontap 기능을 추가한다. 서버에서 불러와야 할텐데. container에는 커버 이미지만 보여준다
+    /// 커버 이미지를 사용자가 정할 수 있는 기능
+    /// 웹도 연결해야 하는가?
+    /// firebase에서 포스팅을 관리해야한다
+    /// stack 혹은 list.builder로 container list를 만든다
+    /// 각 항목에 대한 값을 부여할 때 streambuilder를 통해 이미지 및 내용을 부여한다
+    /// field 값을 출력하는 것 말고 다른 방법은 없는가?
+    /// 새 창의 모습이 블로그 같아야 하는가?
+    /// 사진 - 글 - 사진 - 글 이렇게
+    /// 작성한 포스팅을 이미지 파일로 변환하여 필드값에 넣는다 이를 출력한다
+    /// 다른 방법도 생각해보자
+    /// communityscreen class는 statefulwidget으로 작성하자
+    /// 색은 아이보리 흰색 검정색만 쓰자
+    ///
+    return null;
+  }
+}
